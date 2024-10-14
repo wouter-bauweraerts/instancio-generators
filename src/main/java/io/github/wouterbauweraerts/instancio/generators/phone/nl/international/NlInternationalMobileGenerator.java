05930ff694c4ca6e2f619fbdb4f9347c25a4d610
Wouter Bauweraerts <wouter.bauweraerts@telenet.be>
2024-10-14 17:43:27 +0200

    NL international mobile numbers

--- src/main/java/io/github/wouterbauweraerts/instancio/generators/phone/nl/international/NlInternationalMobileGenerator.java
+++ src/main/java/io/github/wouterbauweraerts/instancio/generators/phone/nl/international/NlInternationalMobileGenerator.java
@@ -1,11 +1,23 @@
 package io.github.wouterbauweraerts.instancio.generators.phone.nl.international;
 
+import org.instancio.Random;
+import org.instancio.generator.Generator;
 import org.instancio.generator.GeneratorContext;
 
-public class NlInternationalMobileGenerator {
+public class NlInternationalMobileGenerator implements Generator<String> {
     private final GeneratorContext ctx;
 
     public NlInternationalMobileGenerator(GeneratorContext ctx) {
         this.ctx = ctx;
+    }
+
+    @Override
+    public String generate(Random random) {
+        return String.format(
+                "+31 6 %s %s %s",
+                random.digits(2),
+                random.digits(2),
+                random.digits(2)
+        );
     }
 }
